7391b3256c58d5e729007fdf4911edcf12df92ec
Saad Mahmud <shuvro.mahmud79@gmail.com>
2023-09-21 11:43:02 +0600

    Increase logo text size to 3xl

--- components/top-bar.js
+++ components/top-bar.js
@@ -1,6 +1,6 @@
 export default function TopBar({ params }) {
   return (
-    <div className="p-4 bg-blue-600 text-gray-100 text-center text-xl font-bold tracking-wide">
+    <div className="p-4 bg-blue-600 text-gray-100 text-center text-3xl font-bold tracking-wide">
       Housechat.ai
     </div>
   );
