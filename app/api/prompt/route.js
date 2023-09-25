ba14e8d73f8f36abdc8c763434604b4678a76ad2
Saad Mahmud <shuvro.mahmud79@gmail.com>
2023-09-25 17:16:31 +0600

    Set runtime to edge

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,6 +4,9 @@ import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { constructPrompt, convertPropertyToContext } from '@/utils/utils';
 
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
