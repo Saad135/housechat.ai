66a455138e2276ba637d9ae046bee9158861ac1a
Saad Mahmud <shuvro.mahmud79@gmail.com>
2023-09-27 00:35:25 +0600

    Add xs navbar buttons

--- components/top-bar.js
+++ components/top-bar.js
@@ -1,7 +1,22 @@
+import ChatSVG from './ui/chat-svg';
+import ListSVG from './ui/list-svg';
+
 export default function TopBar({ params }) {
   return (
     <div className="p-4 bg-blue-600 text-gray-100 text-center text-3xl font-bold tracking-wide">
-      Housechat.ai
+      <div className="flex justify-between sm:block items-baseline px-2">
+        <div className="h-10 w-10 sm:hidden rounded-full hover:bg-blue-500">
+          <button className="hover:bg-blue-500">
+            <ListSVG />
+          </button>
+        </div>
+        <span>Housechat.ai</span>
+        <div className="h-10 w-10 sm:hidden rounded-full hover:bg-blue-500">
+          <button>
+            <ChatSVG />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
